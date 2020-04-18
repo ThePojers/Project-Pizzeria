287e049d5d6bc5df06db9f3ef48aa2dfa6326ba6
Dawid Perzyński <loginosobisty@o2.pl>
2020-04-18 19:52:03 +0200

    Add class CartProduct

--- src/js/script.js
+++ src/js/script.js
@@ -338,9 +338,35 @@
       thisCart.generatedDOM = utils.createDOMFromHTML(generatedHTML);
       console.log(thisCart.generatedDOM);
       thisCart.dom.productList.appendChild(thisCart.generatedDOM);
+      thisCart.products.push(new CartProduct(menuProduct, thisCart.generatedDOM));
+      console.log('thisCart.products', thisCart.products);
     }
   }
 
+  class CartProduct {
+    constructor (menuProduct, element){
+      const thisCartProduct = this;
+
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.amount = menuProduct.amount;
+      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+
+      thisCartProduct.getElements(element);
+      console.log(thisCartProduct);
+    }
+    getElements(element){
+      const thisCartProduct = this;
+      thisCartProduct.dom = {};
+      thisCartProduct.dom.wrapper = element;
+      thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
+    }
+  }
 
   const app = {
     initMenu: function(){
